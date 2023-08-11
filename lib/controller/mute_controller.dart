e0e65887e50bc13de90fe7093438673b936418ad
zamzamnurahman <kazamnn1912@gmail.com>
2023-08-11 11:20:34 +0700

    [add] -mute feature-

--- lib/controller/mute_controller.dart
+++ lib/controller/mute_controller.dart
@@ -1,12 +1,25 @@
+import 'package:flutter_mute/flutter_mute.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final handleMuteProvider =
-    StateNotifierProvider<HandleMuteNotifier, bool>((ref) {
+StateNotifierProvider<HandleMuteNotifier, bool>((ref) {
   return HandleMuteNotifier();
 });
 
 class HandleMuteNotifier extends StateNotifier<bool> {
   HandleMuteNotifier() : super(true);
 
-  changeIsMuted(bool newValue) => state = newValue;
+  changeIsMuted(bool newValue) async{
+    bool isAccessGranted = await FlutterMute.isNotificationPolicyAccessGranted;
+
+    if (!isAccessGranted) {
+      // Opens the notification settings to grant the access.
+      await FlutterMute.openNotificationPolicySettings();
+    }
+    print(await FlutterMute.getRingerMode());
+    await FlutterMute.setRingerMode(state ? RingerMode.Silent: RingerMode.Normal );
+    print(await FlutterMute.getRingerMode());
+
+    state = newValue;
+  }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tajwid_apps/config/theme.dart';
@@ -9,117 +10,171 @@ import 'package:tajwid_apps/screens/quiz_scren.dart';
 
 import 'components/button_circle.dart';
 
-class WelcomeScreen extends ConsumerWidget {
+class WelcomeScreen extends ConsumerStatefulWidget {
   const WelcomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isMute = ref.watch(handleMuteProvider);
-    return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.black,
-          elevation: 0,
-          actions: [
-            IconButton(
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                          backgroundColor: primaryDark,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          content: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  print(isMute);
-                                  ref
-                                      .watch(handleMuteProvider.notifier)
-                                      .changeIsMuted(!isMute);
-                                  Navigator.pop(context);
-                                },
-                                child: ButtonCircle(
-                                  isGradient: false,
-                                  child: Icon(
-                                    isMute
-                                        ? Icons.volume_down
-                                        : Icons.volume_mute,
-                                    color: Colors.white,
+  ConsumerState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends ConsumerState<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    bool isMute = ref.watch(handleMuteProvider);
+    return WillPopScope(
+      onWillPop: () {
+        showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  content: const Text("Yakin Keluar Aplikasi?"),
+                  actions: [
+                    OutlinedButton(
+                        onPressed: Navigator.of(context).pop,
+                        child: const Text(
+                          "Kembali",
+                          style: TextStyle(color: Colors.black),
+                        )),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: primaryColor),
+                        onPressed: () => SystemNavigator.pop(),
+                        child: const Text(
+                          "Keluar",
+                        )),
+                  ],
+                ));
+        return Future.value(true);
+      },
+      child: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.black,
+            elevation: 0,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (_) =>
+                          StatefulBuilder(builder: (context, state) {
+                            return AlertDialog(
+                              backgroundColor: primaryDark,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              content: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      state(() {
+                                        ref
+                                            .watch(handleMuteProvider.notifier)
+                                            .changeIsMuted(!isMute);
+                                        isMute = !isMute;
+                                      });
+                                    },
+                                    child: ButtonCircle(
+                                      isGradient: false,
+                                      child: Icon(
+                                        isMute
+                                            ? Icons.volume_down
+                                            : Icons.volume_mute,
+                                        color: Colors.white,
+                                      ),
+                                    ),
                                   ),
-                                ),
+                                  const ButtonCircle(
+                                    isGradient: false,
+                                    child: Icon(
+                                      Icons.email,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (_) => AboutDialog(
+                                                applicationIcon: Image.asset(
+                                                  "assets/images/illustration_cover.png",
+                                                  height: 50,
+                                                  width: 50,
+                                                ),
+                                                applicationName: "Tajwid Apps",
+                                                applicationVersion: "V.1.0.0",
+                                                children: const [
+                                                  Text(
+                                                      "Tajwid Apps adalah sebuah aplikasi pembelajaran pada pengetahuan Tajwid"),
+                                                ],
+                                              ));
+                                    },
+                                    child: const ButtonCircle(
+                                      isGradient: false,
+                                      child: Icon(
+                                        Icons.info_outline,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              const ButtonCircle(
-                                isGradient: false,
-                                child: Icon(
-                                  Icons.email,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const ButtonCircle(
-                                isGradient: false,
-                                child: Icon(
-                                  Icons.info_outline,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ));
-              },
-              icon: const Icon(Icons.menu),
-            )
-          ],
-        ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: linearGradient,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SvgPicture.asset("assets/images/lafad.svg"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ButtonClickUp(
-                    click: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return const MateriScreen();
-                      }));
-                    },
-                    child: const ButtonCircle(
-                      isGradient: true,
-                      child: Icon(Icons.assignment, size: 30),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  ButtonClickUp(
-                    click: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return const QuizScreen();
-                      }));
-                    },
-                    child: const ButtonCircle(
-                      isGradient: true,
-                      child: Icon(Icons.play_arrow, size: 40),
-                    ),
-                  ),
-                ],
-              ),
-              Image.asset(
-                "assets/images/illustration_cover.png",
-                height: 200,
-              ),
+                            );
+                          }));
+                },
+                icon: const Icon(Icons.menu),
+              )
             ],
           ),
-        ));
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: linearGradient,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SvgPicture.asset("assets/images/lafad.svg"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ButtonClickUp(
+                      click: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return const MateriScreen();
+                        }));
+                      },
+                      child: const ButtonCircle(
+                        isGradient: true,
+                        child: Icon(Icons.assignment, size: 30),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    ButtonClickUp(
+                      click: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return const QuizScreen();
+                        }));
+                      },
+                      child: const ButtonCircle(
+                        isGradient: true,
+                        child: Icon(Icons.play_arrow, size: 40),
+                      ),
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  "assets/images/illustration_cover.png",
+                  height: 200,
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
